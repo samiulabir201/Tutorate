b0477183f6a8525997c4ff69e781fd0fbb9b9ee8
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-04-01 19:07:41 +0600

    Implemented wage check on back-end.

--- Back-End/src/main/java/com/example/tutorate/model/Tutor.java
+++ Back-End/src/main/java/com/example/tutorate/model/Tutor.java
@@ -11,6 +11,7 @@ public class Tutor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private int min_wage;
 
     public Tutor() {}
 
@@ -28,5 +29,13 @@ public class Tutor {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getMin_wage() {
+        return min_wage;
+    }
+
+    public void setMin_wage(int min_wage) {
+        this.min_wage = min_wage;
     }
 }
--- Back-End/src/main/java/com/example/tutorate/service/TutorService.java
+++ Back-End/src/main/java/com/example/tutorate/service/TutorService.java
@@ -1,10 +1,11 @@
 package com.example.tutorate.service;
 
+import com.example.tutorate.model.SearchParams;
 import com.example.tutorate.model.Tutor;
 
 import java.util.List;
 
 public interface TutorService {
     Tutor saveTutor(Tutor tutor);
-    List<Tutor> getTutors(String searchTerm);
+    List<Tutor> getTutors(String searchTerm, SearchParams searchParams);
 }
