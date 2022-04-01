1e583b77afe966cfd9d722b016b487bcc915dc60
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-04-01 19:10:04 +0600

    Updated database.

--- tutorate.sql
+++ tutorate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2022 at 08:03 PM
+-- Generation Time: Apr 01, 2022 at 03:09 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,26 +24,72 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`) VALUES
+(1, 'Chemistry'),
+(2, 'Physics');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tutor`
 --
 
 CREATE TABLE `tutor` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL
+  `name` varchar(255) DEFAULT NULL,
+  `min_wage` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tutor`
 --
 
-INSERT INTO `tutor` (`id`, `name`) VALUES
-(1, 'A'),
-(2, 'B'),
-(3, 'AA');
+INSERT INTO `tutor` (`id`, `name`, `min_wage`) VALUES
+(1, 'A', 0),
+(2, 'B', 10000),
+(3, 'AA', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_subjects`
+--
+
+CREATE TABLE `tutor_subjects` (
+  `tutor_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tutor_subjects`
+--
+
+INSERT INTO `tutor_subjects` (`tutor_id`, `subject_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tutor`
@@ -52,8 +98,20 @@ ALTER TABLE `tutor`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tutor_subjects`
+--
+ALTER TABLE `tutor_subjects`
+  ADD PRIMARY KEY (`tutor_id`,`subject_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tutor`
