b84c03ce0f9d17261c5d8aa3d749ad547abdd106
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-04-06 20:43:18 +0600

    Fixed import error.

--- Back-End/src/main/java/com/example/tutorate/controller/TutorController.java
+++ Back-End/src/main/java/com/example/tutorate/controller/TutorController.java
@@ -7,6 +7,7 @@ import com.example.tutorate.service.TutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
