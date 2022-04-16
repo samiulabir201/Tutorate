a761a90fd5f18dc6ea0d94eb4d5996770b57444b
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-04-16 15:22:43 +0600

    Added back "/rate" endpoint.

--- Back-End/src/main/java/com/example/tutorate/controller/TutorController.java
+++ Back-End/src/main/java/com/example/tutorate/controller/TutorController.java
@@ -1,11 +1,10 @@
 package com.example.tutorate.controller;
 
-import com.example.tutorate.model.Role;
-import com.example.tutorate.model.SearchParams;
-import com.example.tutorate.model.Tutor;
-import com.example.tutorate.model.User;
+import com.example.tutorate.model.*;
+import com.example.tutorate.repository.RatingRepository;
 import com.example.tutorate.repository.TutorRepository;
 import com.example.tutorate.repository.UserRepository;
+import com.example.tutorate.service.RatingService;
 import com.example.tutorate.service.TutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +20,13 @@ public class TutorController {
     @Autowired
     private TutorService tutorService;
     @Autowired
+    private RatingService ratingService;
+    @Autowired
     private UserRepository userRepository;
     @Autowired
     private TutorRepository tutorRepository;
+    @Autowired
+    private RatingRepository ratingRepository;
 
     @PostMapping("/add")
     public User add(@RequestBody Tutor tutor, HttpServletRequest request) {
@@ -85,5 +88,25 @@ public class TutorController {
     @GetMapping("/getAllGrades")
     public List<String> getAllGrades() {
         return tutorRepository.getAllGrades().stream().distinct().collect(Collectors.toList());
+    }
+
+    @GetMapping("/rate")
+    public int rate(@RequestParam int student_id, @RequestParam int tutor_id,@RequestParam int rating) {
+        if (!ratingService.findRated(student_id, tutor_id)) {
+            Tutor selected_tutor = tutorRepository.findById(tutor_id);
+            User selected_user = userRepository.findById(student_id);
+            TutorRatingKey tutorRatingKey = new TutorRatingKey(selected_user, selected_tutor, rating);
+
+            selected_user = ratingService.addRatingRecordStudent(selected_user, tutorRatingKey);
+            selected_tutor = ratingService.addRatingRecordTutor(selected_tutor, tutorRatingKey);
+
+            selected_tutor.calculateAverageRating();
+            ratingRepository.save(tutorRatingKey);
+            tutorRepository.save(selected_tutor);
+            userRepository.save(selected_user);
+
+            return rating;
+        }
+        return 0;
     }
 }
