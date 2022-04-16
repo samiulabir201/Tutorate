2413265373dd4e58590931c6dbdc6fd33ecbe20b
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-04-17 00:25:38 +0600

    Fixed #14.

--- Front-End/src/components/TutorProfileForm.jsx
+++ Front-End/src/components/TutorProfileForm.jsx
@@ -66,10 +66,10 @@ export const TutorProfileForm = (props) => {
                         />
                     </Grid>
                     <Grid item xs={6} className="d-inline-flex">
-                        <Subjects allowNewValues={true} subjects={[]} onSubjectChange={setSubjects} />
+                        <Subjects required allowNewValues={true} subjects={[]} onSubjectChange={setSubjects} />
                     </Grid>
                     <Grid item xs={6} className="d-inline-flex">
-                        <Grades allowNewValues={true} grades={[]} onGradeChange={setGrades} />
+                        <Grades required allowNewValues={true} grades={[]} onGradeChange={setGrades} />
                     </Grid>
                 </Grid>
                 <div className="d-inline-flex mx-auto mt-3">
