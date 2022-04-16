24e808a1c37501bd1522ed35e23296af7d2ca2e0
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-04-17 04:22:24 +0600

    Removed unused entity Subjects.

--- Back-End/src/main/java/com/example/tutorate/repository/SubjectsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tutorate.repository;
-
-import com.example.tutorate.model.Subjects;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectsRepository extends JpaRepository<Subjects, Integer> {
-}
