dba405cc3ae2c322222abd5e8d781dfb94c5d46e
galib130 <galibshams16@gmail.com>
2022-04-18 01:26:26 +0600

    replaced rating with average_rating

--- Front-End/src/components/TutorProfile.jsx
+++ Front-End/src/components/TutorProfile.jsx
@@ -28,11 +28,11 @@ export const TutorProfile = () => {
                 <div className="card-body">
                     <img src={image} className="mx-auto img-fluid rounded-circle mb-2" width="128" height="128" alt=""/>
                     <h4 className="mb-3 fw-bold fs-4 text-center">{tutor.name}</h4>
-                    <p className="mb-2">Rating: {tutor.rating}</p>
                     <p className="mb-2">Location: {tutor.location}</p>
                     <p className="mb-2">Contact: {tutor.phone}</p>
                     <p className="mb-2">Grades: {tutor.grades}</p>
                     <p className="mb-2">Subjects: {tutor.subjects}</p>
+                    <p className="mb-2">Rating: {tutor.average_rating}</p>
                     <p>Fees: BDT {tutor.min_wage}</p>
                     <button className="button" type="submit" 
                     onClick={() => {setFormShown(true)}}
